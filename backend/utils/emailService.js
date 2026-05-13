1ab0aba02ecc0440b3d66b6d1ead0faa202968ee
Praveenkanth2805 <praveenkanth2805@gmail.com>
2026-05-13 22:37:38 +0530

    add port for smpt

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
